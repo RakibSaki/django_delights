e4a06cd0fbb355c13442641ecb235a5b8c079cc6
Rakib Muhammed Saki <rakib1saki@gmail.com>
2022-06-13 21:04:08 +0600

    heroku package 2

--- django_delights/settings.py
+++ django_delights/settings.py
@@ -126,5 +126,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configure Django App for Heroku.
-from .. import django_heroku
+from . import django_heroku
 django_heroku.settings(locals())
